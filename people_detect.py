50531a27ee8e750557e74cf6d47ca1c576a42cac
Carniglia <JuanIgnacio.Carniglia@kcc.com>
2018-04-11 16:29:54 -0300

    Added comments

--- people_detect.py
+++ people_detect.py
@@ -14,10 +14,11 @@ from matplotlib import pyplot as plt
 from scipy.spatial import distance
 import uuid
 
-
+# Function to get Distance between two points (Euclidean Distance)
 def dist(x,y):
     return np.sqrt(np.sum((x-y)**2))
 
+# Function to inform door status.... actually only when it opens
 def sendDoorStatus():
     dataToSend = {
         "Description": "Door Opened",
@@ -25,6 +26,7 @@ def sendDoorStatus():
     }
     sendStatus(dataToSend)
 
+# Function to inform Person Detection
 def sendPeople(description):
     dataToSend = {
         "Description": description,
@@ -32,6 +34,7 @@ def sendPeople(description):
     }
     sendStatus(dataToSend)
 
+# Function to actually do the HTTP Post
 def sendStatus(data):
 
     fmt = "%Y-%m-%d %H:%M:%S.%f"
@@ -52,15 +55,11 @@ def sendStatus(data):
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-#ap.add_argument("-i", "--images", required=True, help="path to images directory")
 args = vars(ap.parse_args())
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-# Camera
-
 
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
@@ -70,7 +69,7 @@ if args.get("video", None) is None:
 else:
     camera = cv2.VideoCapture(args["video"])
 
-
+# Set some camera parameters
 cam_params = {
     'brightness': camera.get(11), #32.0,
     'contrast': camera.get(12), #64.0,
@@ -80,10 +79,9 @@ cam_params = {
     'exposure': camera.get(16), #,
     }
 
-#cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
-#cv2.namedWindow('Before', cv2.WINDOW_NORMAL)
 cv2.namedWindow('After', cv2.WINDOW_NORMAL)
 
+# These will store the place where the user last clicked
 ix,iy = -1,-1
 # mouse callback function
 def getMousePoint(event,x,y,flags,param):
@@ -99,33 +97,43 @@ def getMousePoint(event,x,y,flags,param):
         print(danger_area_pts1)
 
 selectedPoint = 1
+
+# let the hardcoding begin...
+# define danger / safe areas ... will use later on
 danger_area_pts1 = np.array([[89,2],[198,3],[177,246],[79,247]], np.int32)
 danger_area_pts2 = np.array([[381,1],[497,1],[436,248],[336,248]], np.int32)
 exit_area_pts = np.array([[203,1],[380,1],[336,248],[183,248]], np.int32)
+
+# define where we do the person detection
 exit_area_detection = (180,00,371,154)
+
+# our "Marker" for detecting open door... I use the handle (see video)
 coord_door_open = (175, 118, 178, 122)
 status_door = False
 
 cv2.setMouseCallback('After',getMousePoint)
 numPeople = 0
 frame_counter = 0
-
 lastFound = np.array((0,0,0))
 door_status_sent = False
+
+# this is part of the hack for not sending multiple HTTP Posts for the same
+# person
 personSentAt = time.time()
 
 while(True):
     ret, frame = camera.read()
-    frame_counter += 1
 
+    # little code for looping on the video
+    frame_counter += 1
     if frame_counter == camera.get(cv2.CAP_PROP_FRAME_COUNT):
         frame_counter = 0 #Or whatever as long as it is the same as next line
         camera.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    image = imutils.resize(frame, width=500)
-    orig = frame.copy()
 
-    #door open detection
+    image = imutils.resize(frame, width=500)
+
+    # begin Door Open Detection
     door_handle_gray = image[coord_door_open[1]:coord_door_open[3], coord_door_open[0]:coord_door_open[2]]
     avg = door_handle_gray.mean()
     if avg < 100:
@@ -137,16 +145,13 @@ while(True):
         status_door = False
         door_status_sent = False
 
+    # end Door Open Detection
 
 
-
-    #people detection
+    # begin People Detection
     image_for_detection = image[exit_area_detection[1]:exit_area_detection[3], exit_area_detection[0]:exit_area_detection[2]]
-
-    #cv2.imshow("handle", image_for_detection)
     (rects, weights) = hog.detectMultiScale(image_for_detection, winStride=(2, 2),
     padding=(2, 2), scale=1.05)
-
 
     rects = np.array([[x + exit_area_detection[0],
                        y + exit_area_detection[1],
@@ -154,6 +159,7 @@ while(True):
                        y + exit_area_detection[1] + h] for (x, y, w, h) in rects])
 
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.05)
+    # end People Detection
 
     cnts = np.array([exit_area_pts])
 
@@ -161,13 +167,13 @@ while(True):
         cv2.rectangle(image, (xA, yA), (xB, yB), (250, 255, 100), 2)
         cv2.circle(image, (xA+int((xB-xA)/2) , yA+int((yB-yA)/2)), 10, (250, 250, 250), -1 )
 
+
+        # Check to see if the person was at the indicated zone
         coef = int((yB-yA) * 0.2)
         ret = cv2.pointPolygonTest(cnts, (xA + int((xB-xA) / 2), yB-coef), False)
         if ret > 0:
-            #p = np.array((xA, yA,0))
-            #distance = dist(lastFound,p)
-            #print("Distance: {}".format(distance))
-            #if distance > 25:
+            # Check to see is not too soon to send a new HTTP Post (If it is,
+            # it means is the same person)
             if (time.time() - personSentAt) > 2:
                 cv2.rectangle(image, (xA, yA), (xB, yB), (0, 0, 255), 2)
                 lastFound = np.array((xA, yA,0))
@@ -177,10 +183,9 @@ while(True):
                 personSentAt = time.time()
                 sendPeople("Person detected on video")
 
-            #break
-        else:
-            cv2.rectangle(image, (xA, yA), (xB, yB), (100, 255, 100), 1)
+        cv2.rectangle(image, (xA, yA), (xB, yB), (100, 255, 100), 1)
 
+    # Draw Danger / Safe zones
     cv2.polylines(image, np.array([danger_area_pts1]), True, (0,0,255), 2)
     cv2.polylines(image, np.array([danger_area_pts2]), True, (0,0,255), 2)
 
